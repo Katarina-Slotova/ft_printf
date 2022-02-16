d9c96fcbbe6474eb2f3680f108d6d7425bac2aeb
Katarina-Slotova <katarina.slotova@gmail.com>
2022-02-16 20:29:44 +0200

    Added test file.

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "ft_printf.h"
+
+int main ()
+{
+    char *x = "hello";
+    //const char * y = "monkeys";
+
+	//int x = 12;
+	//int *ptr = &x;
+	unsigned int num = 25670;
+	printf("The address is: %.19p.\n", x);
+    /* Demonstrate with numbers. */
+    //ft_printf ("<%s> is not justified.\n", x);
+    //printf ("<%5d> is right-justified.\n", x);
+    //printf ("<%-2d> The minus sign makes it left-justified.\n", x);
+    /* Demonstrate with strings. */
+    //printf ("'%s' is not justified.\n", y);
+    //printf ("'%10s' is right-justified.\n", y);
+    //printf ("'%-10s' is left-justified using a minus sign.\n", y);
+
+    return 0;
+}
